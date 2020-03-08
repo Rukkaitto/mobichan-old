ea459db8b65663e634e308f89c33221c637b5246
Lucas Goudin <lucas.goudin@etu.u-bordeaux.fr>
2020-03-08 17:59:26 +0100

    Added image zoom and pan

--- lib/screens/image_viewer_screen.dart
+++ lib/screens/image_viewer_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:extended_image/extended_image.dart';
 
 class ImageViewerScreen extends StatelessWidget {
   String board, ext;
@@ -10,10 +12,75 @@ class ImageViewerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xaa000000),
-      body: GestureDetector(
-        onTap: () => Navigator.pop(context),
-        child: Center(
-          child: Image.network('https://i.4cdn.org/$board/$imageId$ext'),
+      body: Builder(
+        builder: (context) => Stack(
+          alignment: Alignment.topRight,
+          children: <Widget>[
+            Center(
+              child: ExtendedImage.network(
+                'https://i.4cdn.org/$board/$imageId$ext',
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                fit: BoxFit.contain,
+                mode: ExtendedImageMode.gesture,
+                initGestureConfigHandler: (state) {
+                  return GestureConfig(
+                    minScale: 1.0,
+                    animationMinScale: 0.7,
+                    maxScale: 3.0,
+                    animationMaxScale: 3.5,
+                    speed: 1.0,
+                    inertialSpeed: 100.0,
+                    initialScale: 1.0,
+                    inPageView: false,
+                    cacheGesture: true,
+                    initialAlignment: InitialAlignment.center,
+                  );
+                },
+              ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    MaterialButton(
+                      padding: EdgeInsets.zero,
+                      minWidth: 0.0,
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      child: Icon(
+                        Icons.save,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      onPressed: () {
+                        Scaffold.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Saved'),
+                            duration: Duration(seconds: 1),
+                          ),
+                        );
+                      },
+                    ),
+                    MaterialButton(
+                      padding: EdgeInsets.zero,
+                      minWidth: 0.0,
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      child: Icon(
+                        Icons.close,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
